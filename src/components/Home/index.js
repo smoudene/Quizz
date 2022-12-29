027ced2c1e7586b2d3497cb414ab4962877738fd
salah eddine moudene <smoudene25@gmail.com>
2022-12-29 15:35:15 +0100

    add questions

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,50 +1,13 @@
 import React,{useState, useEffect} from 'react'
 import questions from  "./quizz"
+import "./style.css"
 
 const Home = () => {
   const [index, setIndex] = useState(0);
   const [score, setScore] = useState(0);
   const [show, setShow] = useState(true);
 
-    const questions = 
-    [
-        {
-            step:1,
-            question : "question 1",
-            choice1: "answer 1",
-            choice2: "answer 2",
-            choice3: "answer 3",
-            choice4: "answer 4",
-            right: "answer 1"
-         },
-          {
-            step:2,
-            question : "question 2",
-            choice1: "answer 1",
-            choice2: "answer 2",
-            choice3: "answer 3",
-            choice4: "answer 4",
-            right: "answer 2"
-         },
-          {
-            step:3,
-            question : "question 3",
-            choice1: "answer 1",
-            choice2: "answer 2",
-            choice3: "answer 3",
-            choice4: "answer 4",
-            right: "answer 3"
-         },
-          {
-            step:4,           
-            question : "question 4 ",
-            choice1: "answer 1",
-            choice2: "answer 2",
-            choice3: "answer 3",
-            choice4: "answer 4",
-            right: "answer 4"
-        },
-      ]
+   
       const next = (e) => {
          setIndex(step => step < questions.length -1 ? step + 1 : step)
          if(e.target.value == e.target.name){
@@ -57,20 +20,28 @@ const Home = () => {
       const step = questions[index]
     
   return (
-    <div>
+    <div className="container grid h-screen place-items-center">
+      
     { show ?
 
-    <div class="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow-md sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
+    <div className="w-96 p-4 bg-white border border-gray-200 rounded-lg shadow-md sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
         
-         <h5 class="text-xl font-medium text-gray-900 dark:text-white mb-6">{step.question}</h5>
-         <button type="button" class="w-full text-white bg-gray-500 hover:bg-green-700 font-medium rounded-lg text-sm px-5 py-2.5 my-2 text-center" value={step.choice1} name={step.right} onClick={e => next(e, "value", "name")}>{step.choice1}</button>
-        <button type="button" class="w-full text-white bg-gray-500 hover:bg-green-700 font-medium rounded-lg text-sm px-5 py-2.5 my-2 text-center" value={step.choice2} name={step.right} onClick={e => next(e, "value")}>{step.choice2}</button>
-        <button type="button" class="w-full text-white bg-gray-500 hover:bg-green-700 font-medium rounded-lg text-sm px-5 py-2.5 my-2 text-center" value={step.choice3} name={step.right} onClick={e => next(e, "value")}>{step.choice3}</button>
-        <button type="button" class="w-full text-white bg-gray-500 hover:bg-green-700 font-medium rounded-lg text-sm px-5 py-2.5 my-2 text-center" value={step.choice4} name={step.right} onClick={e => next(e, "value")}>{step.choice4}</button>
+         <h5 className="text-xl font-medium text-gray-900 dark:text-white mb-6">{step.question}</h5>
+         <button type="button" className="w-full text-white bg-gray-500 hover:bg-green-700 font-medium rounded-lg text-sm px-5 py-2.5 my-2 text-center" value={step.choice1} name={step.right} onClick={e => next(e, "value", "name")}>{step.choice1}</button>
+        <button type="button" className="w-full text-white bg-gray-500 hover:bg-green-700 font-medium rounded-lg text-sm px-5 py-2.5 my-2 text-center" value={step.choice2} name={step.right} onClick={e => next(e, "value")}>{step.choice2}</button>
+        <button type="button" className="w-full text-white bg-gray-500 hover:bg-green-700 font-medium rounded-lg text-sm px-5 py-2.5 my-2 text-center" value={step.choice3} name={step.right} onClick={e => next(e, "value")}>{step.choice3}</button>
+        <button type="button" className="w-full text-white bg-gray-500 hover:bg-green-700 font-medium rounded-lg text-sm px-5 py-2.5 my-2 text-center" value={step.choice4} name={step.right} onClick={e => next(e, "value")}>{step.choice4}</button>
     </div> :
-     <div>
-      <p>your score is : {score} </p>
-     </div>
+    <div className=" grid w-96 p-6 bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700 place-items-center">
+    <a href="#">
+        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{score >= questions.length / 2 ? "You did it congratulations ,  " : "Oops..."}</h5>
+    </a>
+    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">your score is : {score} / {questions.length}</p>
+    <a href="/" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+        Play Again 
+        <svg aria-hidden="true" className="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+    </a>
+</div>
     }
     </div>
 
--- src/components/Home/quizz.js
+++ src/components/Home/quizz.js
@@ -1,11 +1,103 @@
-const questions = [
+const questions = 
+[
     {
-        question : "hhhhhhhhh",
-        choice1: "c1",
-        choice1: "c2",
-        choice1: "c3",
-        choice1: "c4",
-        right: "c4"
+        step:1,
+        question : "من هو صاحب أسرع هاترك في الدوري الإنجليزي الممتاز؟",
+        choice1: "Alan Shearar",
+        choice2: "Sadio Mane",
+        choice3: "Harry Kane",
+        choice4: "Luis suarez",
+        right: "Sadio Mane"
+     },
+      {
+        step:2,
+        question : "في أي بطولة كأس عالم سجل فيها دييغو مارادونا هدفه الشهير باليد؟",
+        choice1: "Mexico 1986",
+        choice2: "Italy 1990",
+        choice3: "France 1998",
+        choice4: "USA 1994",
+        right: "Mexico 1986"
+     },
+      {
+        step:3,
+        question : "اعتزل ميسي دوليًا في أي عام قبل عكس قراره؟",
+        choice1: "2014",
+        choice2: "2015",
+        choice3: "2016",
+        choice4: "2017",
+        right: "2016"
+     },
+      {
+        step:4,           
+        question : "من هو أكثر لاعب سجل في نسخة واحدة من كأس أمم أوروبا؟ (Euro)",
+        choice1: "Michel platini",
+        choice2: "Cristiano Ronaldo",
+        choice3: "Thierry Henry",
+        choice4: "Fernando Torres",
+        right: "Michel platini"
     },
-]
+    {
+        step:5,           
+        question : "متى كانت النسخة الأولى من كأس أمم أوروبا؟",
+        choice1: "1962",
+        choice2: "1960",
+        choice3: "1966",
+        choice4: "1970",
+        right: "1960"
+    },
+    {
+        step:6,           
+        question : "بعد يوفنتوس، ميلان وإنتر، أي فريق فاز بأكثر ألقاب الدوري الإيطالي؟",
+        choice1: "Torino",
+        choice2: "Napoli",
+        choice3: "Genova",
+        choice4: "Roma",
+        right: "Genova"
+    },
+    {
+        step:7,           
+        question : "أي فريق فاز بلقب الدوري الإنجليزي الأول؟",
+        choice1: "Man United",
+        choice2: "Aston Villa",
+        choice3: "Chelsea",
+        choice4: "Arsenal",
+        right: "Man United"
+    },
+    {
+        step:8,           
+        question : "من سجل أسرع هدف سجل في تاريخ الدوري الإنجليزي؟",
+        choice1: "Mo Salah",
+        choice2: "Kun Aguero",
+        choice3: "Shane Long",
+        choice4: "Peter Crouch",
+        right: "Shane Long"
+    },
+    {
+        step:9,           
+        question : "من هو النادي الوحيد، غير ريال مدريد وبرشلونة الذي لم يلعب خارج دوري الدرجة الأولى الإسباني؟",
+        choice1: "Valencia",
+        choice2: "Atleti Bilbao",
+        choice3: "Atleti Madrid",
+        choice4: "Sevilla",
+        right: "Atleti Bilbao"
+    },
+    {
+        step:10,           
+        question : "أي حارس مرمى لديه أفضل سجل في الدوري الإنجليزي الممتاز؟",
+        choice1: "Van Der Sar",
+        choice2: "Peter Cech",
+        choice3: "Joe Hart",
+        choice4: "Jordan Pickford",
+        right: "Peter Cech"
+    },
+    {
+        step:11,           
+        question : "من هو الفريق الأول من المملكة المتحدة الذي فاز بكأس أوروبا؟",
+        choice1: "Man United",
+        choice2: "Rangers",
+        choice3: "Celtic",
+        choice4: "Aston Villa",
+        right: "Celtic"
+    },
+  ]
 export default questions
